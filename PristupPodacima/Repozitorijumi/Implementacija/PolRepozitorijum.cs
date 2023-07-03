f2b127479ba8278879f7c0ebacf830c347fbbaa5
Tamara1406 <tamaram0053@gmail.com>
2023-07-03 20:44:38 +0200

    Kreirani repozitorijumi

---
+++ PristupPodacima/Repozitorijumi/Implementacija/PolRepozitorijum.cs
@@ -0,0 +1,44 @@
+﻿using Domen;
+using PristupPodacima.Repozitorijumi.Interfejsi;
+
+namespace PristupPodacima.Repozitorijumi.Implementacija
+{
+    internal class PolRepozitorijum : IPolRepozitorijum
+    {
+        Context context;
+        public PolRepozitorijum(Context context)
+        {
+            this.context = context;
+        }
+
+        public void Dodaj(Pol t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Izmeni(Pol t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Obrisi(Pol t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pol> Pretrazi(string kriterijum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pol Vrati(int t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pol> VratiSve()
+        {
+            return context.Pol.ToList();
+        }
+    }
+}
---
+++ PristupPodacima/Repozitorijumi/Implementacija/TerminRepozitorijum.cs
@@ -0,0 +1,45 @@
+﻿using Domen;
+using Microsoft.EntityFrameworkCore;
+using PristupPodacima.Repozitorijumi.Interfejsi;
+
+namespace PristupPodacima.Repozitorijumi.Implementacija
+{
+    public class TerminRepozitorijum : ITerminRepozitorijum
+    {
+        Context context;
+        public TerminRepozitorijum(Context context)
+        {
+            this.context = context;
+        }
+
+        public void Dodaj(Termin t)
+        {
+            context.Termini.Add(t);
+        }
+
+        public void Izmeni(Termin t)
+        {
+            context.Termini.Update(t);
+        }
+
+        public void Obrisi(Termin t)
+        {
+            context.Termini.Remove(t);
+        }
+
+        public List<Termin> Pretrazi(string kriterijum)
+        {
+            return context.Termini.Where(t => t.Grupa.GrupaIme == kriterijum).ToList();
+        }
+
+        public Termin Vrati(int id)
+        {
+            return context.Termini.Single(t => t.TerminID == id);
+        }
+
+        public List<Termin> VratiSve()
+        {
+            return context.Termini.Include(t => t.Grupa).ToList();
+        }
+    }
+}
