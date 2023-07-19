c88ff4c93fba7c889a2f217f3fa815b726dc16c1
Tamara1406 <tamaram0053@gmail.com>
2023-07-19 18:34:42 +0200

    Promenjena baza

--- PristupPodacima/Context.cs
+++ PristupPodacima/Context.cs
@@ -16,7 +16,7 @@ namespace PristupPodacima
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Aplikacija.NP;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Aplikacija.teretana;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
